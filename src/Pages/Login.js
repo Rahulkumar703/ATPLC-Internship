50e67c5a28124b7cac6ad2a7a53b17d447a032f0
Rahul Kumar <rahulkumar703@outlook.com>
2023-03-12 22:00:43 +0530

    fixed login message box

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -69,7 +69,7 @@ export default function Login() {
                 </div>
                 {
                     error !== '' &&
-                    <div className="error-messages">
+                    <div className="message-box">
                         {error}
                     </div>
                 }
