f6ac2aac9523e00e5a9d5d4f4de06eb6297d24ed
Rahul Kumar <rahulkumar703@outlook.com>
2023-03-28 12:17:15 +0530

    added checks in enroll page

--- src/Pages/Enroll/Enroll.js
+++ src/Pages/Enroll/Enroll.js
@@ -10,6 +10,7 @@ export default function Enroll() {
     const location = useLocation();
     const { id, courseName, courseDuration, coverImage, couresPrice, courseTechnologies } = location?.state;
     const [message, setMessage] = useState('');
+    const [isloading, setIsloading] = useState(false);
     const [user, setUser] = useState({
         name: '',
         email: '',
@@ -38,63 +39,73 @@ export default function Enroll() {
 
     const displayRazorPay = async (e) => {
         e.preventDefault();
-        const res = await loadScript(
-            "https://checkout.razorpay.com/v1/checkout.js"
-        );
-
-        if (!res) {
-            setMessage("Razorpay SDK failed to load. Are you online?");
-            return;
+        setIsloading(true);
+        if (user.name === '' || user.email === '' || user.message === '') {
+            setMessage('Please fill all details')
         }
-
-        // creating a new order
-        const result = await axios.post(`${process.env.REACT_APP_BACKEND_PATH}/course-order`, { amount: couresPrice * 100 });
-
-        if (!result) {
-            setMessage("Server error. Are you online?");
-            return;
+        else if (user.password !== user.confirmPassword) {
+            setMessage('Password not mathced');
         }
+        else {
+            const res = await loadScript(
+                "https://checkout.razorpay.com/v1/checkout.js"
+            );
 
-        // Getting the order details back
-        const { amount, order_id, currency } = result.data;
+            if (!res) {
+                setMessage("Razorpay SDK failed to load. Are you online?");
+                return;
+            }
 
-        const options = {
-            key: process.env.REACT_APP_RAZORPAY_API, // Enter the Key ID generated from the Dashboard
-            amount: amount.toString(),
-            currency: currency,
-            name: "ATPLC",
-            description: "Test Transaction",
-            image: 'https://www.atplc.in/Assets/Images/atplc_logo.png',
-            order_id: order_id,
-            handler: async function (response) {
-                const data = {
-                    razorpay_paymentId: response.razorpay_payment_id,
-                    razorpay_orderId: response.razorpay_order_id,
-                    razorpay_signature: response.razorpay_signature,
-                    Name: user.name,
-                    Email: user.email,
-                    Password: user.password
-                };
-                const result = await axios.post(`${process.env.REACT_APP_BACKEND_PATH}/payment-success`, data);
+            // creating a new order
+            const result = await axios.post(`${process.env.REACT_APP_BACKEND_PATH}/course-order`, { amount: couresPrice * 100 });
 
-                console.log(result.data);
+            if (!result) {
+                setMessage("Server error. Are you online?");
+                return;
+            }
 
-                setMessage(result.data.res);
-            },
-            notes: {
-                courseId: id,
-                courseName,
-                name: user.name,
-                email: user.email,
-                date: Date.now()
-            },
-            theme: {
-                color: "#9c394b",
-            },
-        };
+            // Getting the order details back
+            const { amount, order_id, currency } = result.data;
 
-        const paymentObject = new window.Razorpay(options);
-        paymentObject.open();
+            const options = {
+                key: process.env.REACT_APP_RAZORPAY_API, // Enter the Key ID generated from the Dashboard
+                amount: amount.toString(),
+                currency: currency,
+                name: "ATPLC",
+                description: "Test Transaction",
+                image: 'https://www.atplc.in/Assets/Images/atplc_logo.png',
+                order_id: order_id,
+                handler: async function (response) {
+                    const data = {
+                        razorpay_paymentId: response.razorpay_payment_id,
+                        razorpay_orderId: response.razorpay_order_id,
+                        razorpay_signature: response.razorpay_signature,
+                        Name: user.name,
+                        Email: user.email,
+                        Password: user.password
+                    };
+                    const result = await axios.post(`${process.env.REACT_APP_BACKEND_PATH}/payment-success`, data);
+
+                    console.log(result.data);
+
+                    setMessage(result.data.res);
+                },
+                notes: {
+                    courseId: id,
+                    courseName,
+                    name: user.name,
+                    email: user.email,
+                    date: Date.now()
+                },
+                theme: {
+                    color: "#9c394b",
+                },
+            };
+
+            const paymentObject = new window.Razorpay(options);
+            paymentObject.open();
+        }
+        setIsloading(false);
     }
 
     const handelChange = (e) => {
@@ -103,7 +114,6 @@ export default function Enroll() {
 
     return (
         <section className='page enroll-page'>
-            {message !== '' && <div className="message-box">{message}</div>}
             <div className="page-heading">
                 <h3>Enroll Course</h3>
             </div>
@@ -178,11 +188,12 @@ export default function Enroll() {
 
                 <form onSubmit={displayRazorPay} className="payment-form">
                     <div className="form-container">
+                        {message !== '' && <div className="message-box">{message}</div>}
                         <Input type="text" label={'Full Name'} id='name' name='name' icon={'fi fi-rr-user'} value={user.name} onChange={handelChange} />
                         <Input type="email" label={'Email'} id='email' name='email' icon={'fi fi-rr-user'} value={user.email} onChange={handelChange} />
                         <Input type="password" label={'Password'} id='password' name='password' icon={'fi fi-rr-user'} value={user.password} onChange={handelChange} />
                         <Input type="password" label={'Confirm Password'} id='confirmPassword' name='confirmPassword' icon={'fi fi-rr-user'} value={user.confirmPassword} onChange={handelChange} />
-                        <Button label="Enroll Now" icon={"fi fi-rr-wallet"} type="submit" />
+                        <Button label="Enroll Now" icon={"fi fi-rr-wallet"} type="submit" isLoading={isloading} />
                     </div>
                 </form>
             </div>
